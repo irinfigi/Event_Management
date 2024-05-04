f26e9e9759310b0c33a5a9ccec839758c186306d
Irin F Mampilly <irinfigi123@gmail.com>
2024-05-04 19:25:26 +0530

    modified:   eventpr/eventapp/__pycache__/urls.cpython-312.pyc 	new file:   eventpr/eventapp/__pycache__/views.cpython-312.pyc 	new file:   eventpr/eventapp/migrations/__pycache__/__init__.cpython-312.pyc 	modified:   eventpr/eventapp/urls.py 	modified:   eventpr/eventapp/views.py 	modified:   eventpr/eventpr/__pycache__/settings.cpython-312.pyc

--- eventpr/eventapp/views.py
+++ eventpr/eventapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request,'index.html')
+def about(request):
+    return render(request,'about.html')
+def events(request):
+    return render(request,'events.html')
+def contact(request):
+    return render(request,'contact.html')
+def booking(request):
+    return render(request,'booking.html')
