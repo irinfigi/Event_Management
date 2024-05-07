b354cd53dc2ea5186ae44e7dd846987232d4521e
Irin F Mampilly <irinfigi123@gmail.com>
2024-05-07 21:46:26 +0530

    modified:   eventpr/eventapp/views.py

--- eventpr/eventapp/views.py
+++ eventpr/eventapp/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
-
+from .models import Event
 # Create your views here.
 def index(request):
     return render(request,'index.html')
 def about(request):
     return render(request,'about.html')
 def events(request):
-    return render(request,'events.html')
+    dict_eve={
+        'eve':Event.objects.all()
+    }
+    return render(request,'events.html',dict_eve)
 def contact(request):
     return render(request,'contact.html')
 def booking(request):
