800518613ac0f19c6e0950d17fb5a29a7847f372
Irin F Mampilly <irinfigi123@gmail.com>
2024-05-07 22:58:15 +0530

    modified:   eventpr/eventapp/__pycache__/models.cpython-312.pyc 	modified:   eventpr/eventapp/models.py

--- eventpr/eventapp/models.py
+++ eventpr/eventapp/models.py
@@ -5,3 +5,9 @@ class Event(models.Model):
     img=models.ImageField(upload_to="pic")
     name=models.CharField(max_length=50)
     desc=models.CharField(max_length=100)
+class Booking(models.Model):
+    cus_name=models.CharField(max_length=55)
+    cus_ph=models.CharField(max_length=15)
+    name=models.ForeignKey(Event,on_delete=models.CASCADE)
+    booking_date=models.DateField()
+    booked_on=models.DateField(auto_now=True)
